2de9bdd7ee6637eaf71cd70ecaa9a9519a054a68
matthew shearer <>
2018-09-05 14:48:55 +1200

    move toolbar to bottom

--- app/src/main/java/com/shearer/jetmoviedb/features/main/MainActivity.kt
+++ app/src/main/java/com/shearer/jetmoviedb/features/main/MainActivity.kt
@@ -59,7 +59,7 @@ class MainActivity : AppCompatActivity() {
     private fun hideKeyboard(hideKeyboard: Boolean) {
         if (hideKeyboard) {
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(editText.windowToken, 0)
+            imm.hideSoftInputFromWindow(container.windowToken, 0)
         }
 
     }
