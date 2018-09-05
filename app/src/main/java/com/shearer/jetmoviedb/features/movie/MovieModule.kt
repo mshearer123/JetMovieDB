e9fe3e7781d5ccabe66d2c20c7ddfc9f8cfb59ba
mshearer123 <mshearer123@gmail.com>
2018-09-05 13:52:51 +1200

    update database migration strategy to be destructive (#17)

--- app/src/main/java/com/shearer/jetmoviedb/features/movie/MovieModule.kt
+++ app/src/main/java/com/shearer/jetmoviedb/features/movie/MovieModule.kt
@@ -16,7 +16,10 @@ import org.koin.dsl.module.module
 
 
 val movieModule = module {
-    single { Room.databaseBuilder(get(), MovieDb::class.java, "movie-db").build().movies() }
+    single { Room.databaseBuilder(get(), MovieDb::class.java, "movie-db")
+            .fallbackToDestructiveMigration()
+            .build()
+            .movies() }
     factory { MovieRepositoryDefault(get()) as MovieRepository }
     factory { MovieDbRepositoryDefault(get()) as MovieDbRepository }
     factory { MovieInteractorDefault(get(), get()) as MovieInteractor }
